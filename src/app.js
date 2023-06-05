99d1b0944052436a0d5682cb00606eed46bce883
Gabriel Ortega <o.gabriel18@hotmail.com>
2023-06-04 21:45:43 -0300

    Primera entrega

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from 'express';
+
 import productRoutes from './Routes/product.routes.js';
 import cartRoutes from './Routes/cart.routes.js';
 
-const app = express();
 const port = 8080;
+const app = express();
+
 
 app.use('/api/products', productRoutes);
 app.use('/api/carts', cartRoutes);
